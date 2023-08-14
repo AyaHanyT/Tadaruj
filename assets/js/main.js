61fe9f24fee0cffc8d783880806c9de2d817d772
AyaHanyT <ayahtarabay@gmail.com>
2023-08-14 21:27:15 +0300

    10-Tadaruj

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+$("#toggler").click(function () {
+  document.getElementById("mySidenav").style.cssText =
+  "margin-right : 0 ; width : 100vw"
+})
+
+$(".close-btn").click(function () {
+  document.getElementById("mySidenav").style.cssText =
+  "margin-right : -100vw ; width : 0"
+})
+
+
+//   $("#toggler").click(function () {
+//   document.getElementById("mySidenav").style.cssText =
+//   "margin-right : 0 ; width : 100vw"
+//   document.getElementById("page").style.marginRight = "0"
+// })
+
+// function closeNav() {
+//   document.getElementById("mySidenav").style.cssText =
+//     "margin-right : -100vw ; border-left: none"
+//   document.getElementById("page").style.paddingRight = "0"
+// }
+
+// function mobileSize() {
+//   document.getElementById("mySidenav").style.cssText =
+//     "margin-right : 0 ; width : 100vw"
+//   document.getElementById("page").style.marginRight = "0"
+// }
+
+
+
